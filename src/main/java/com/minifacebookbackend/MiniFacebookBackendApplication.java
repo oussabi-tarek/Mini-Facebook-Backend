7b976f7bff7d02301882403a032667bafc95b5fb
oussabi-tarek <tarek.oussabi@edu.uca.ma>
2023-09-20 21:26:28 +0100

    feat:project initialisation

---
+++ src/main/java/com/minifacebookbackend/MiniFacebookBackendApplication.java
@@ -0,0 +1,13 @@
+package com.minifacebookbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniFacebookBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniFacebookBackendApplication.class, args);
+    }
+
+}
