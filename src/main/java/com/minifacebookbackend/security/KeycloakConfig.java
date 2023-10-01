b45098a65ca2793576fc4922cdc2cbdc662175ad
abdessalamboulayat <abdessalam6boulayat@gmail.com>
2023-10-01 11:57:17 +0100

    spring config and keycloak config

---
+++ src/main/java/com/minifacebookbackend/security/KeycloakConfig.java
@@ -0,0 +1,13 @@
+package com.minifacebookbackend.security;
+
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KeycloakConfig {
+    @Bean
+    public KeycloakSpringBootConfigResolver springBootConfigResolver(){
+        return new KeycloakSpringBootConfigResolver();
+    }
+}
--- src/main/java/com/minifacebookbackend/security/SecurityConfig.java
+++ src/main/java/com/minifacebookbackend/security/SecurityConfig.java
@@ -1,4 +1,45 @@
 package com.minifacebookbackend.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity
+                .csrf(csrf->csrf.disable())
+                .authorizeHttpRequests(request -> request.requestMatchers("/auth/login/**").permitAll())
+                .authorizeHttpRequests(request -> request.requestMatchers("/auth/register/**").permitAll())
+                .authorizeHttpRequests(request -> request.anyRequest().authenticated());
+        httpSecurity
+                .oauth2ResourceServer(auth -> auth.jwt(Customizer.withDefaults()));
+        return httpSecurity.build();
+    }
+    @Bean
+    public SessionAuthenticationStrategy sessionAuthenticationStrategy(){
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+    /*@Bean
+    public AuthenticationManager authenticationManager(){
+        KeycloakAuthenticationProvider keycloakAuthenticationProvider = new KeycloakAuthenticationProvider();
+        return new ProviderManager(keycloakAuthenticationProvider);
+    }*/
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 }
